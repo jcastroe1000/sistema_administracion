47ad99c4e212c8ec762e283a117aee98af3ecf35
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2016-02-15 11:44:31 -0600

    chage name to the file upload and adding validations to the images

--- header.php
+++ header.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>CRUD Galeria</title>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>CRUD Galeria</title>
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/dataTables.bootstrap.css" rel="stylesheet">
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/dataTables.bootstrap.css" rel="stylesheet">
 
-    
-    
-  </head>
-  <body>
-  
-    <p><br/></p>
-    <div class="container">
-      
-	    <p><br/></p>
-	    <div class="panel panel-default">
-	      <div class="panel-body">
+
+
+    </head>
+    <body>
+
+        <p><br/></p>
+        <div class="container">
+
+            <p><br/></p>
+            <div class="panel panel-default">
+                <div class="panel-body">
--- js/upload.js
+++ js/upload.js
@@ -1,62 +1,85 @@
-$(function(){
-	$('#subida').submit(function(){
-		
-		var comprobar = $('#title').val().length *$('#foto').val().length * $('#desc_short').val().length *$('#desc_long').val().length*$('#status').val().length *$('#creation_date').val().length  ;
-		
-		if(comprobar>0){
-			
-			var formulario = $('#subida');
-			
-			var datos = formulario.serialize();
-			
-			var archivos = new FormData();	
-			
-			var url = 'php/subir_foto.php';
-			
-				for (var i = 0; i < (formulario.find('input[type=file]').length); i++) { 
-				
-               	 archivos.append((formulario.find('input[type="file"]:eq('+i+')').attr("name")),((formulario.find('input[type="file"]:eq('+i+')')[0]).files[0]));
-				 
-      		 	}
-				
-			$.ajax({
-				
-				url: url+'?'+datos,
-				
-				type: 'POST',
-				
-				contentType: false, 
-				
-            	data: archivos,
-				
-               	processData:false,
-				
-				beforeSend : function (){
-					
-					$('#cargando').show(300);	
-				
-				},
-				success: function(data){
-					
-					$('#cargando').hide(300);
-					
-					$('#fotos').append(data);
-					
-					$('#subida')[0].reset();
-					
-					return false;
-				}
-				
-			});
-			
-			return false;
-			
-		}else{
-			
-			alert('Selecciona una foto para subir e ingrese su descripcion');
-			
-			return false;
-			
-		}
-	});
+$(function () {
+    $('#subida').submit(function () {
+        var comprobar = $('#title').val().length * $('#foto').val().length * $('#desc_short').val().length * $('#desc_long').val().length * $('#status').val().length * $('#creation_date').val().length;
+
+        if (comprobar > 0) {
+
+            var formulario = $('#subida');
+
+            var datos = formulario.serialize();
+
+            var archivos = new FormData();
+
+            var url = 'php/Upload_Photo.php';
+
+            for (var i = 0; i < (formulario.find('input[type=file]').length); i++) {
+
+                archivos.append((formulario.find('input[type="file"]:eq(' + i + ')').attr("name")), ((formulario.find('input[type="file"]:eq(' + i + ')')[0]).files[0]));
+
+            }
+
+            $.ajax({
+                url: url + '?' + datos,
+                type: 'POST',
+                contentType: false,
+                data: archivos,
+                processData: false,
+                beforeSend: function () {
+
+                    $('#cargando').show(300);
+
+                },
+                success: function (data) {
+
+                    $('#cargando').hide(900);
+                    $(location).attr('href', 'Galery_Photos.php');
+
+                    return false;
+                }
+
+            });
+
+            return false;
+
+        } else {
+
+
+            var imagen = document.getElementById("foto").files;
+            if (imagen.length == 0)
+
+            {
+                $('#foto').after('<div class="alert alert-danger">No has seleccionado ningun archivo</div>');
+                return false;
+            }
+            else
+
+            {
+
+                for (x = 0; x < imagen.length; x++)
+
+                {
+
+
+
+                    if (imagen[x].type != "image/png" && imagen[x].type != "image/jpg" && imagen[x].type != "image/jpeg" && imagen[x].type != "image/gif")
+
+                    {
+                        $('#foto').after('<div class="alert alert-danger">El archivo ' + imagen[x].name + '  no es una imagen, selecciona una imagen</div>');
+
+                        return false;
+
+                    }
+
+
+                    if (imagen[x].size > 1024 * 1024 * 3)
+
+                    {
+                        $('#foto').after('<div class="alert alert-danger">La imagen ' + imagen[x].name + '  pesa mas de 3 MB selescciona otro archivo</div>');
+                        return  false;
+                    }
+                }
+            }
+
+        }
+    });
 });
