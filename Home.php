475c834ca1f2716cf2adb01b813d3026bc50e13e
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2016-02-17 16:36:51 -0600

    modifying the query to the database and adding file to conect to the DB

--- Home.php
+++ Home.php
@@ -12,7 +12,7 @@ $mail = $_SESSION['user_name'];
 
 $mysqli = mysqli_connect('localhost', 'root', '', 'sistem_galery');
 
-$query2 = "SELECT * FROM Users WHERE user='$mail'";
+$query2 = "SELECT nombre FROM Users WHERE user='$mail'";
 $res = mysqli_query($mysqli, $query2);
 $num_row = mysqli_num_rows($res);
 $row = mysqli_fetch_array($res);
--- index.php
+++ index.php
@@ -1,33 +1,27 @@
 <?php
-session_start();
-
-
-if (isset($_SESSION['user_name']) != "") {
-    header("Location: Home.php");
-}
-
-if (isset($_POST['btn-login'])) {
-    $mysqli = mysqli_connect('localhost', 'root', '', 'sistem_galery');
-
-    $email = mysql_real_escape_string($_POST['email']);
-    $pass = mysql_real_escape_string($_POST['pass']);
-
-    $email = trim($email);
-
-    $query = "SELECT * FROM Users WHERE user='$email' AND password='$pass'";
-    $result = mysqli_query($mysqli, $query)or die(mysqli_error());
-    $num_row = mysqli_num_rows($result);
-    $row = mysqli_fetch_array($result);
-    if ($num_row >= 1) {
-        echo 'true';
-        $_SESSION['user_name'] = $row['user'];
-        header("Location:Home.php");
-    } else {
-        ?>
+     include "config.php";
+     session_start();
+    if (isset($_SESSION['user_name']) != "") {
+        header("Location: Home.php");
+    }
+    if (isset($_POST['btn-login'])) {
+        $email = mysql_real_escape_string($_POST['email']);
+        $pass = mysql_real_escape_string($_POST['pass']);
+        $email = trim($email);
+        $query = "SELECT user FROM Users WHERE user='$email' AND password='$pass'";
+        $result = mysqli_query($mysqli, $query)or die(mysqli_error());
+        $num_row = mysqli_num_rows($result);
+        $row = mysqli_fetch_array($result);
+        if ($num_row >= 1) {
+            echo 'true';
+            $_SESSION['user_name'] = $row['user'];
+            header("Location:Home.php");
+        } else {
+?>
         <script>alert('Username / Password Seems Wrong !');</script>
         <?php
+        }
     }
-}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
