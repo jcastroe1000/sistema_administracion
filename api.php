ecbaa77616123eb950b69c4393542a4c459e2836
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2016-02-20 13:37:22 -0600

    adding GET method for adding value gallery,adding validation to existing gallery or not

--- api.php
+++ api.php
@@ -1,11 +1,15 @@
 <?php
     include "config.php";
     header("Content-Type: application/json; charset=UTF-8");
+    error_reporting(0);
+
+    $valor = $_GET["valor"];
+    
     $result = $mysqli->query("SELECT id_galery,title_galery,short_description,long_description,creation_date,modification_date, "
-            . "status FROM galery WHERE id_galery = 17");
+            . "status FROM galery WHERE id_galery = '".$valor."'");
     while($row = $result->fetch_array(MYSQLI_ASSOC)) {
         $response ['galery'] = array(
-        //'id_gallery' => $row['id_galery'],
+        'id_gallery' => $row['id_galery'],
         'title' => $row['title_galery'],
         'short_description' => $row['short_description'],
         'long_description' => $row['long_description'],
@@ -14,13 +18,16 @@
         'modification_date' => $row['modification_date'],
         );  
     }
+    
+    
     $result2 = $mysqli2->query("SELECT route,title,short_description,long_description from 
         content left join content_galery on content.id_content = content_galery.id_content where 
-        content_galery.id_galery = 17");
+        content_galery.id_galery = '".$valor."'");
     while($row = $result2->fetch_array(MYSQLI_ASSOC)) {
       $partialImage = array(
           
-          'path' => $row['route'],
+          'path' => 'php/album/' . $row['route'],
+          'title' => $row ['title'],
           'short_description' => $row['short_description'],
           'long_description' => $row['long_description'],
           
@@ -28,10 +35,14 @@
     array_push($response['galery'], $partialImage);
 
     }
-    $json2 = json_encode($response['galery']);
-    echo $json2;
-//    $json2 = json_encode($response);
-//    echo $json2;
+    if($response!=null){
+        $json2 = json_encode($response['galery']);
+        echo $json2;
+    }  else {
+        echo 'no se encontraron resultados';
+    }
+    
+    
 
 
 
